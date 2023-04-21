a7331bcdfbe92737cff2b536dad1e607860e35e8
Douglas Moore <doug.moore@oddball.io>
2023-04-21 15:03:31 -0400

    testing

---
+++ auth.sh
@@ -0,0 +1,22 @@
+ECWS-v3 521784486762 learn-application-admin
+flh-dev 405622158906 flh-developer-admin
+flh-test 504465124926 flh-developer-admin
+learn-dev 329043178936 learn-application-admin
+learn-prod 840218319247 learn-application-admin
+mapi-dev 204718834091 mapi-developer-admin
+mapi-test 395677180642 mapi-developer-admin
+pc2-dev 155987839255 pc2-developer-admin
+pc2-test 115900149718 pc2-developer-admin
+scout-dev 198854277681 scout-application-admin
+scout-impl 394953402894 scout-application-admin
+scout-prod 263813120251 scout-application-admin
+scout-test 001265600887 scout-application-admin
+wdsops-dev 879613780019 wdsops-developer-admin
+wdsops-prod 251875634914 wdsops-application-admin
+FLH-v3 136935787478 ?
+OCF Dev 573629285115 ct-ado-ocf-developer-admin (not working)
+OCF Impl 706569442529 ct-ado-ocf-developer-admin (not working)
+OCF Test 270478921721 ct-ado-ocf-developer-admin (not working)
+
+
+kion console --account-id 879613780019 --cloud-access-role wdsops-developer-admin
